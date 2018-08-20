808cf687abde12e825d70bfb02d3b5cd82e13afe
Risma <Risma@192.168.56.1>
2018-08-20 13:23:18 +0700

    jobsheet1

--- src/Latihan/Variabel.java
+++ src/Latihan/Variabel.java
@@ -14,7 +14,7 @@ public class Variabel {
     int nilai_pbo,nilai_web;
     nilai_pbo = 90;
     nilai_web = 80;
-    System.out.println("Variabel yang terdapat dalam program :");
+    System.out.println("Variabel yang terdapat dalam program ");
     System.out.println("nilai_pbo = " + nilai_pbo);
     System.out.println("nilai_web = " + nilai_web);
     }
